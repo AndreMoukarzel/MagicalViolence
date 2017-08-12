23373652ab45c9c35588f528070e6d43d5d4af72
LuizGyro <luizfelipe.g@hotmail.com>
2017-08-11 23:21:52 -0300

    Continue Developing Controller Support and Test Scene
    Added a connection test and visual representation.

--- Scripts/Test Scripts/ControllerTest.gd
+++ Scripts/Test Scripts/ControllerTest.gd
@@ -1,6 +1,16 @@
 
 extends Control
 
+var connected_joysticks = []
+
 
 func _ready():
+	set_fixed_process(true)
 
+func _fixed_process(delta):
+	for node in get_node("Controllers").get_children():
+		node.set_texture(load("res://Sprites/TestSprites/NoController.png"))
+	
+	connected_joysticks = Input.get_connected_joysticks()
+	for joy in connected_joysticks:
+		get_node(str("Controllers/", joy)).set_texture(load(str("res://Sprites/TestSprites/Yes", joy, "Controller.png")))
