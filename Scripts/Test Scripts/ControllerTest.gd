f006ea42b0cb11b1a338decc820df8ff1f6360e4
LuizGyro <luizfelipe.g@hotmail.com>
2017-08-13 00:02:03 -0300

    Continue Developing Controller Test Scene
    Added face button and left joystick tests.

--- Scripts/Test Scripts/ControllerTest.gd
+++ Scripts/Test Scripts/ControllerTest.gd
@@ -13,4 +13,36 @@ func _fixed_process(delta):
 	
 	connected_joysticks = Input.get_connected_joysticks()
 	for joy in connected_joysticks:
+		#print (Input.is_joy_known(joy))
 		get_node(str("Controllers/", joy)).set_texture(load(str("res://Sprites/TestSprites/Yes", joy, "Controller.png")))
+		
+		# Face button check
+		
+		if (Input.is_joy_button_pressed(joy, JOY_BUTTON_0)):
+			get_node(str("FaceButtons/", joy, "/A")).set_texture(load("res://Sprites/TestSprites/APressed.png"))
+		else:
+			get_node(str("FaceButtons/", joy, "/A")).set_texture(load("res://Sprites/TestSprites/ANormal.png"))
+		
+		if (Input.is_joy_button_pressed(joy, JOY_BUTTON_1)):
+			get_node(str("FaceButtons/", joy, "/B")).set_texture(load("res://Sprites/TestSprites/BPressed.png"))
+		else:
+			get_node(str("FaceButtons/", joy, "/B")).set_texture(load("res://Sprites/TestSprites/BNormal.png"))
+			
+		if (Input.is_joy_button_pressed(joy, JOY_BUTTON_2)):
+			get_node(str("FaceButtons/", joy, "/X")).set_texture(load("res://Sprites/TestSprites/XPressed.png"))
+		else:
+			get_node(str("FaceButtons/", joy, "/X")).set_texture(load("res://Sprites/TestSprites/XNormal.png"))
+			
+		if (Input.is_joy_button_pressed(joy, JOY_BUTTON_3)):
+			get_node(str("FaceButtons/", joy, "/Y")).set_texture(load("res://Sprites/TestSprites/YPressed.png"))
+		else:
+			get_node(str("FaceButtons/", joy, "/Y")).set_texture(load("res://Sprites/TestSprites/YNormal.png"))
+			
+		# Left Stick Axis Check
+		
+		var horizontal = Input.get_joy_axis(joy, JOY_AXIS_0) * 37
+		var vertical = Input.get_joy_axis(joy, JOY_AXIS_1) * 37
+		
+		#print (str("h = ", horizontal))
+		#print (str("v = ", vertical))
+		get_node(str("LeftJoystick/", joy, "/Center")).set_pos(Vector2(horizontal + 132, vertical + 320))
