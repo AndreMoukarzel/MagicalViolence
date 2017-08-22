2f0cb05fcbe65385b2640d53cf2d8e0f70fe9462
Luiz Girotto <luizfelipe.g@hotmail.com>
2017-08-22 08:11:17 -0300

    Added up command to MainMenu

--- Scripts/MainMenu.gd
+++ Scripts/MainMenu.gd
@@ -8,19 +8,35 @@ func _ready():
 	set_process_input(true)
 	
 func _input(event):
+	
 	if event.is_action_pressed("ui_down") or event.is_action_pressed("ui_right"):
 		menu_pos = (menu_pos + 1) % menu_order.size()
 		
-		get_node(str(menu_order[menu_pos], "/ButtonAnimations")).play("Hovering")
+		get_node(str(menu_order[menu_pos], "/ButtonAnimations")).play("HoveringDown")
+		set_process_input(false)
+		yield(get_node(str(menu_order[menu_pos], "/ButtonAnimations")), "finished")
+		set_process_input(true)
+	
+	if event.is_action_pressed("ui_up") or event.is_action_pressed("ui_left"):
+		menu_pos = (menu_pos - 1) % menu_order.size()
+		
+		get_node(str(menu_order[menu_pos], "/ButtonAnimations")).play("HoveringUp")
 		set_process_input(false)
 		yield(get_node(str(menu_order[menu_pos], "/ButtonAnimations")), "finished")
 		set_process_input(true)
 		
 ################### Animation Functions ###################
 		
-func adjust_z_leaving():
+func adjust_z_leaving_down():
 	var hover = get_node(str(menu_order[(menu_pos - 1) % menu_order.size()], "/Hover"))
 	var normal = get_node(str(menu_order[(menu_pos - 1) % menu_order.size()], "/Normal"))
+	
+	hover.set("z/z", hover.get("z/z") - 1)
+	normal.set("z/z", normal.get("z/z") + 1)
+	
+func adjust_z_leaving_up():
+	var hover = get_node(str(menu_order[(menu_pos + 1) % menu_order.size()], "/Hover"))
+	var normal = get_node(str(menu_order[(menu_pos + 1) % menu_order.size()], "/Normal"))
 	
 	hover.set("z/z", hover.get("z/z") - 1)
 	normal.set("z/z", normal.get("z/z") + 1)
