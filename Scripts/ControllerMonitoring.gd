a1bcf3c8c59f9de67da1f82e6037195b05c0f5bc
Luiz Girotto <luizfelipe.g@hotmail.com>
2017-08-28 15:44:23 -0300

    Continue developing controller monitoring, repurposed by design decisions

--- Scripts/ControllerMonitoring.gd
+++ Scripts/ControllerMonitoring.gd
@@ -1,23 +1,43 @@
-extends Control
+extends Node
 
-var controller_ports = []
 
 # This is to be called at the beggining of the game (it is loaded globally, and is a singleton),
 # so we initialize controller_ports on ready(), then any changes made are caught by the Input signal.
 func _ready():
 	Input.connect("joy_connection_changed", self, "joysticks_changed")
 	
-	# Initialize controller_ports
-	var connected_joysticks = Input.get_connected_joysticks()
-	
-	if (global.keyboard_enabled == true):
-		controller_ports.append("Keyboard")
-		
-		for num in range (0, connected_joysticks.size()):
-			if (controller_ports.size() >= 4):
-				break
-			controller_ports.append(connected_joysticks[num])
+	# So we acknowledge controllers connected on game startup
+	for joy in Input.get_connected_joysticks():
+		joysticks_changed(joy, true)
 	
 func joysticks_changed(index, connected):
-	print (str("Index = ", index))
-	print (str("Connected = ", connected))
+	# Por ora, vamos assumir que temos uma estrutura de árvore
+	# linear, e que o node mais relevante é o último. Ou podemos
+	# adicionar o node como filho da árvore, pode ser também.
+	
+	if (connected):
+		# Check if a known joystick
+		if not Input.is_joy_known(index):
+			# Instance Warning
+			var warning_scn = load("res://Scenes/Warnings/ControllerWarning.tscn")
+			var wrn_scn = warning_scn.instance()
+			add_child(wrn_scn)
+			get_tree().set_pause(true)
+		
+		else:
+			# Give menu controls to the device
+			var default_config = ConfigFile.new()
+			if (default_config.load(str("user://default.cfg")) != OK):
+				print ("Error, could not load default data!")
+				return
+			
+			for key in default_config.get_section_keys("Menu Button"):
+				var value = default_config.get_value("Menu Button", key)
+				var event = InputEvent()
+				
+				event.type = InputEvent.JOYSTICK_BUTTON
+				event.device = index
+				event.button_index = value
+				
+				InputMap.action_add_event(key, event)
+		
