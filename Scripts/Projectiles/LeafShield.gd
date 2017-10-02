1652a6d3e241d0650d699584705897dbf4c4bbaa
vitorkei <vitor_kei9@hotmail.com>
2017-10-02 15:42:34 -0300

    Updating

--- Scripts/Projectiles/LeafShield.gd
+++ Scripts/Projectiles/LeafShield.gd
@@ -92,8 +92,11 @@ func die():
 			child.die()
 	parent.spell_ended()
 	set_process( false )
-	get_node("Timer").start()
+#	get_node("Timer").start()
 
 
 func _on_Timer_timeout():
-	queue_free()
+	parent.spell_ended()
+	set_process(false)
+	get_node("AnimationPlayer").play("death")
+	leaf_count = 0
