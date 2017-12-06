d083724a85c631a6134eded5cc4d87753a68803a
Luiz Girotto <luizfelipe.g@hotmail.com>
2017-12-06 14:00:04 -0200

    Corrected SRGB profile, begin adapting BattleTest

--- Scripts/Test Scripts/BattleTest.gd
+++ Scripts/Test Scripts/BattleTest.gd
@@ -4,18 +4,21 @@ extends Node
 var positions = [Vector2(200, 300), Vector2(800, 300), Vector2(510, 100), Vector2(510, 500)]
 var char_scn = preload("res://Scenes/Character.tscn")
 
+var character_sprites = []
+
 var living = -1
 
 func _ready():
 	get_parent().get_node("Loading Animation").hide()
 
-func start(players_num):
-	living = players_num
-	
-	for i in range(players_num):
+func start(active_players):
+	living = active_players.size()
+
+	for port in active_players:
 		var char_inst = char_scn.instance()
-		char_inst.set_name(str("Character", i))
-		char_inst.set_pos(positions[i])
+		char_inst.set_name(str("Character", port))
+		char_inst.set_pos(positions[port])
+		set_animation(character_sprites[port])
 		char_inst.connect("death", self, "anotherOneBitesTheDust")
 		add_child(char_inst)
 
