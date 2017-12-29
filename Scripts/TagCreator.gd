b8e9356399b2ca4fe15d662889b27bd984cc86d8
Luiz Girotto <luizfelipe.g@hotmail.com>
2017-12-29 11:18:49 -0200

    Minor adjusments to TagCreator

--- Scripts/TagCreator.gd
+++ Scripts/TagCreator.gd
@@ -13,11 +13,13 @@ var scroll_counter = 0
 var scroll_speed = 0.3
 var tag = ""
 
-var toprow = ["X", "Y", "Z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W"]
+var toprow = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 var toprow_node_order = []
 
-var leftmost_toprow = 0
-var rightmost_toprow = 6
+# Need to set this on ready() (or initialize) using
+# something like toprow.size() - floor(toprow.size/2)
+var leftmost_toprow = toprow.size() - 3
+var rightmost_toprow = 3
 
 # IMPORTANT!
 # The leftmost and rightmost labels are buffers. They are never shown, and
@@ -40,10 +42,10 @@ func _ready():
 	set_fixed_process(true)
 	for child in get_node("TopRow").get_children():
 		toprow_node_order.append(child)
-	
+
 	controller_monitor.controller_ports[0] = 1000
 	controller_monitor.map_css_controls(0, "default")
-	
+
 	btn_left = input_states.new(name_adapter("css_left", 0))
 	btn_right = input_states.new(name_adapter("css_right", 0))
 	btn_accept = input_states.new(name_adapter("css_accept", 0))
@@ -73,35 +75,35 @@ func _fixed_process(delta):
 func move_left():
 	var symbol_amount = toprow.size()
 	var next_leftmost
-	
+
 	# Done here se we can set the correct text later
 #	rightmost_toprow = (leftmost_toprow + symbol_amount - 1) % symbol_amount
 	rightmost_toprow = (rightmost_toprow + 1) % symbol_amount
 	leftmost_toprow = (leftmost_toprow + 1) % symbol_amount
-	
+
 	for child in toprow_node_order:
 		# Make all white
 		child.set("custom_colors/font_color", Color(255, 255, 255))
-		
+
 		# Is leftmost node
 		if (toprow_node_order.find(child) == 0):
 			child.set_pos(Vector2(180, 0))
-			
+
 			child.set_text(toprow[rightmost_toprow])
 		else:
 			child.get_node("Tween").interpolate_property(child, "rect/pos", child.get_pos(), child.get_pos() - Vector2(30, 0), scroll_speed, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 			child.get_node("Tween").start()
-	
+
 	# yield here
 	set_fixed_process(false)
 	for child in toprow_node_order:
 		# We choose one that is guaranteed to tween.
 		if (toprow_node_order.find(child) == 1):
 			yield(child.get_node("Tween"), "tween_complete")
-	
+
 	# Mudar a cor do node central. Agora fica facil, só pegar o node que é
 	# floor(toprow_node_order.size() / 2)
-	
+
 	shift_left_toprow()
 	toprow_node_order[floor(toprow_node_order.size() / 2)].add_color_override("font_color", Color(255, 0, 0))
 	adjust_scroll()
@@ -110,34 +112,34 @@ func move_left():
 func move_right():
 	var symbol_amount = toprow.size()
 	var next_leftmost
-	
+
 	# Done here se we can set the correct text later
 	rightmost_toprow = (rightmost_toprow + symbol_amount - 1) % symbol_amount
 	leftmost_toprow = (leftmost_toprow + symbol_amount - 1) % symbol_amount
-	
+
 	for child in toprow_node_order:
 		# Make all white
 		child.set("custom_colors/font_color", Color(255, 255, 255))
-		
+
 		# Is leftmost node
 		if (toprow_node_order.find(child) == (toprow_node_order.size() - 1)):
 			child.set_pos(Vector2(0, 0))
-			
+
 			child.set_text(toprow[leftmost_toprow])
 		else:
 			child.get_node("Tween").interpolate_property(child, "rect/pos", child.get_pos(), child.get_pos() + Vector2(30, 0), scroll_speed, Tween.TRANS_LINEAR, Tween.EASE_OUT)
 			child.get_node("Tween").start()
-	
+
 	# yield here
 	set_fixed_process(false)
 	for child in toprow_node_order:
 		# We choose one that is guaranteed to tween.
 		if (toprow_node_order.find(child) == 1):
 			yield(child.get_node("Tween"), "tween_complete")
-	
+
 	# Mudar a cor do node central. Agora fica facil, só pegar o node que é
 	# floor(toprow_node_order.size() / 2)
-	
+
 	shift_right_toprow()
 	toprow_node_order[floor(toprow_node_order.size() / 2)].add_color_override("font_color", Color(255, 0, 0))
 	adjust_scroll()
@@ -145,18 +147,18 @@ func move_right():
 
 func shift_left_toprow():
 	var leftmost
-	
+
 	leftmost = toprow_node_order[0]
 	toprow_node_order.pop_front()
-	
+
 	toprow_node_order.append(leftmost)
 
 func shift_right_toprow():
 	var rightmost
-	
+
 	rightmost = toprow_node_order[toprow_node_order.size() - 1]
 	toprow_node_order.pop_back()
-	
+
 	toprow_node_order.push_front(rightmost)
 
 func adjust_scroll():
@@ -173,17 +175,17 @@ func select_symbol():
 	var middle_symbol = toprow[floor((leftmost_toprow + number_to_mid) % toprow.size())]
 	tag = str(tag, middle_symbol)
 	get_node("Tag").set_text(tag)
-	
+
 func save_tag():
 	var dir = Directory.new()
 	var tag_name
 	var file_name
 	var existent_tags = []
-	
+
 	# Do not allow empty tags
 	if (tag == ""):
 		return
-	
+
 	# Populate existent tags
 	if (dir.open("user://") == OK):
 		dir.list_dir_begin()
@@ -196,7 +198,7 @@ func save_tag():
 			file_name = dir.get_next()
 	else:
 		print ("Directory not found. Something went wrong.")
-		
+
 	# Check if is not a repeat, create a config file
 	if (existent_tags.find(tag) == -1):
 
